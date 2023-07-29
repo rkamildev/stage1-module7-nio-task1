0dec78b03f5105de8172229fa346168e255475ea
rkamildev <rkamil.int@gmail.com>
2023-07-29 19:54:21 +0500

    NIO prac

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path path = Paths.get(String.valueOf(file));
+        Profile profile = new Profile();
+        String line;
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"))) {
+            while ( (line = reader.readLine()) != null) {
+                String[] str = line.split(":\\s+");
+                if (str[0].equals("Name"))
+                    profile.setName(str[1]);
+                if (str[0].equals("Age"))
+                    profile.setAge(Integer.parseInt(str[1]));
+                if (str[0].equals("Email"))
+                    profile.setEmail(str[1]);
+                if (str[0].equals("Phone"))
+                    profile.setPhone(Long.parseLong(str[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
